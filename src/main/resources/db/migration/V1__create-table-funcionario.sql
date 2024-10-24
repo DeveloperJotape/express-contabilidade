a54bbac1aa2f41a89ca74720c8729a30d0c10014
João Pedro <developerjotape@gmail.com>
2024-10-24 11:58:31 -0300

    Fix: migration Funcionario

--- src/main/resources/db/migration/V1__create-table-funcionario.sql
+++ src/main/resources/db/migration/V1__create-table-funcionario.sql
@@ -2,12 +2,12 @@ CREATE TABLE Funcionario(
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
-    dataNascimento DATE NOT NULL,
+    data_nascimento DATE NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(100) NOT NULL,
     telefone VARCHAR(11),
     cargo VARCHAR(30) NOT NULL,
     situacao VARCHAR(7) NOT NULL,
-    dataEntrada DATE NOT NULL,
-    dataSaida DATE
+    data_entrada DATE NOT NULL,
+    data_saida DATE
 );
