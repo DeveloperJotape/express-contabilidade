360062894796373c9acdbe37c1935d7b3c627247
João Pedro <developerjotape@gmail.com>
2024-10-24 12:16:33 -0300

    Fix: Tipo Date alterado para VARCHAR

--- src/main/resources/db/migration/V1__create-table-funcionario.sql
+++ src/main/resources/db/migration/V1__create-table-funcionario.sql
@@ -2,12 +2,12 @@ CREATE TABLE Funcionario(
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
-    data_nascimento DATE NOT NULL,
+    data_nascimento VARCHAR(30) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(100) NOT NULL,
     telefone VARCHAR(11),
     cargo VARCHAR(30) NOT NULL,
     situacao VARCHAR(7) NOT NULL,
-    data_entrada DATE NOT NULL,
-    data_saida DATE
+    data_entrada VARCHAR(30) NOT NULL,
+    data_saida VARCHAR(30)
 );
